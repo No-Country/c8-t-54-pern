2b8768031e474a58437df4810cf7711514410503
Ignacio Perez <ignacio.perez.jobs@gmail.com>
2022-11-14 18:07:49 -0300

    remove comments

--- client/src/app/state/authSlice.ts
+++ client/src/app/state/authSlice.ts
@@ -14,26 +14,11 @@ export const authSlice = createSlice({
   initialState: EmptyUserState,
   reducers: {
     login: (state,action) => {
-        console.log(state)
     }
-/*     createUser: (state, action) => {
-      persistLocalStorage<UserInfo>(UserKey, action.payload);
-      return action.payload;
-    },
-    updateUser: (state, action) => {
-      const result = { ...state, ...action.payload };
-      persistLocalStorage<UserInfo>(UserKey, result);
-      return result;
-    },
-    resetUser: () => {
-      clearLocalStorage(UserKey);
-      return EmptyUserState;
-    } */
+
   }
 });
 
 export const {login} = authSlice.actions
-/* 
-export const { createUser, updateUser, resetUser } = userSlice.actions; */
 
 export default authSlice.reducer;

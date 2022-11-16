2c00a8a7594f34c8ab6570491419a8033eacf614
Gonzalo Nicolás Argüello <arguellogonzalo97@gmail.com>
2022-11-16 17:42:37 -0300

    persistence path ok

--- server/models/products.ts
+++ server/models/products.ts
@@ -1,33 +1,33 @@
-import db from '../utils/database.util';
-import { DataTypes } from 'sequelize';
+const { db } = require("../utils/database.util");
+import { DataTypes } from "sequelize";
 
 const columns = {
-    id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        allowNull: false,
-        primaryKey: true
-    },
-    productName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    description: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    quantityInStock: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    },
-    price: {
-        type: DataTypes.DECIMAL,
-        allowNull: false,
-    }
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true,
+  },
+  productName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  quantityInStock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+  },
 };
 
 const config = {};
 
-const Product = db.define('Product', columns, config);
+const Product = db.define("Product", columns, config);
 
-module.exports = {Product}
+module.exports = { Product };

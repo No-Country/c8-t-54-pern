93469a79bd8961af8bc2d6b011a45ba32727f000
gaspar-costa-calvo <gaspar.c.calvo@gmail.com>
2022-11-17 10:34:49 -0300

    Starting with CRUD

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express"
 import { Response, Request } from "express";
+import productRouter from "./products";
 import userRouter from "./users";
 const router = Router()
 
 router.use("/users", userRouter);
+router.get('/products', productRouter);
 router.get("/", (_req: Request, res: Response) => res.send("OK"));
 
 export default router;

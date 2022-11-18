51bb63a5a01fcf276212ca8442b049670f55b47d
gaspar-costa-calvo <gaspar.c.calvo@gmail.com>
2022-11-18 11:54:10 -0300

    Fixed a little possibly bugs

--- server/app.ts
+++ server/app.ts
@@ -4,7 +4,7 @@ const app = express();
 import indexRouter from "./routes/index"
 
 app.use(cors());
-app.set('port', process.env.PORT || 5432);
+app.set('port', process.env.PORT || 3000);
 app.set('host', process.env.HOST || process.env.CLIENT_URL || 'http://localhost');
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())

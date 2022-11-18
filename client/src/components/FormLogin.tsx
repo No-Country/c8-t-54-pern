b04dca767a4b765810d0aec3f86e8de1d330c366
Ignacio Perez <ignacio.perez.jobs@gmail.com>
2022-11-18 13:23:56 -0300

    settings of style Login page and component form

--- client/src/components/FormLogin.tsx
+++ client/src/components/FormLogin.tsx
@@ -29,11 +29,11 @@ const FormLogin: React.FC<IProps> = ({input,setInput}:IProps) => {
 
   return (
     <form className="flex flex-col gap-5 relative  w-4/5 sm:w-full lg:items-center" onSubmit={handleSubmit}>
-        <input type="email" name="email" placeholder="Ingresa tu E-mail" className="p-3 placeholder:rgba(170,170,170,1) drop-shadow sm:w-full lg:w-4/5" onChange={handleChange} value={input.email}/>
-        <input type="password" name="password" placeholder="ingresa tu contraseña" className="p-3 placeholder:rgba(170,170,170,1) drop-shadow sm:w-full lg:w-4/5" onChange={handleChange} value={input.password}/>
+        <input type="email" name="email" placeholder="Ingresa tu E-mail" className="p-2 placeholder:rgba(170,170,170,1) rounded-md drop-shadow sm:w-full lg:w-4/5  border-[0.1px] placeholder:font-medium" onChange={handleChange} value={input.email}/>
+        <input type="password" name="password" placeholder="ingresa tu contraseña" className="p-2 placeholder:rgba(170,170,170,1) rounded-md drop-shadow sm:w-full lg:w-4/5  border-[0.1px] placeholder:font-medium" onChange={handleChange} value={input.password}/>
         <p className='text-gray-400'>¿Olvidaste tu contraseña?</p>
-        <button className="bg-zinc-700 text-white p-3 rounded-sm text-lg hover:bg-white hover:text-black hover: border-4 border-zinc-700 lg:w-4/5 sm:w-full">Iniciar sesion</button>
-        <button className="rounded-sm border-4 border-zinc-700 p-3 hover:bg-zinc-700 hover:text-white cursor-pointer flex items-center justify-center gap-2 sm:w-full lg:w-4/5 ">
+        <button className="bg-[#3A3A3A] text-white p-2 rounded-md text-lg hover:bg-white hover:text-[#383838] hover: border-[3px] border-zinc-700 lg:w-4/5 sm:w-full">Iniciar sesion</button>
+        <button className="rounded-md border-[3px] border-[#3A3A3A] p-2 hover:bg-[#3A3A3A] text-[#383838] hover:text-white cursor-pointer flex items-center justify-center gap-2 sm:w-full lg:w-4/5 ">
           <FcGoogle className="text-xl"/>
           Iniciar sesión con Google
         </button>
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,28 +1,32 @@
 import { useState } from "react";
 import FormLogin from "../components/FormLogin";
 import { UserInfo } from "../models/userInfo";
-import {Link} from 'react-router-dom'
-interface stateForm  {
-  user:UserInfo
+import { Link } from "react-router-dom";
+interface stateForm {
+  user: UserInfo;
 }
 
 function Login() {
-  const [input, setInput] = useState<stateForm['user']>({
-    email:"",
+  const [input, setInput] = useState<stateForm["user"]>({
+    email: "",
     password: "",
   });
   return (
-    <main className="flex h-screen justify-center items-center sm:w-full md:gap-4 lg:gap-6">
-      <div className=" w-2/5 h-5/6 bg-hero-login bg-no-repeat bg-cover bg-center justify-center items-center hidden md:block"></div>
-      <div className="items-center flex gap-4 flex-col sm:w-full md:w-2/5 ">
-        <div className="w-4/5 sm:-w-full">
+    <main className="flex h-screen justify-center items-center sm:w-full md:gap-4 lg:gap-6 ">
+      <div className=" w-2/4 h-5/6 md:h-[700px] bg-hero-login bg-no-repeat bg-cover bg-center justify-center items-center hidden md:block"></div>
+      <div className="items-center flex  flex-col sm:w-full gap-4 md:w-2/5 md:h-auto md:gap-2 lg:gap-4 lg:w-[30%] lg:h-[70%] xl:w-2/5 xl:h-[full] xl:justify-center ">
+        <div className="w-4/5 sm:-w-full flex flex-col md:gap-3">
           <h2 className="text-3xl font-bold">Inicia sesión</h2>
-          <p className="text-gray-800 ">¡Qué bueno tenerte de nuevo en MOVEment!</p>
+          <p className=" font-medium text-sm">
+            ¡Qué bueno tenerte de nuevo en MOVEment!
+          </p>
         </div>
-          <FormLogin input={input} setInput={setInput}/>
+        <FormLogin input={input} setInput={setInput} />
         <div className="flex gap-3">
           <p>¿No tienes una cuenta?</p>
-          <Link to={'/register'} className="font-semibold">Crea tu cuenta</Link>
+          <Link to={"/register"} className="font-semibold">
+            Crea tu cuenta
+          </Link>
         </div>
       </div>
     </main>

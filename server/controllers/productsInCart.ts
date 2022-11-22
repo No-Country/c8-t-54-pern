701a0f6f04604d086d1bf7216beb82d34de72436
Gonzalo Nicolás Argüello <arguellogonzalo97@gmail.com>
2022-11-22 17:35:26 -0300

    addToCart v1.1

--- server/controllers/productsInCart.ts
+++ server/controllers/productsInCart.ts
@@ -1,17 +1,23 @@
 const { Product } = require("../models/Products");
 const { User } = require("../models/Users");
-const {Cart} = require("../models/Carts")
+const { Cart } = require("../models/Carts");
 import { Request, Response } from "express";
 
+export const test = async (req: Request, res: Response) => {
+  res.status(200).json({ Funciona: "Si funciona" });
+};
+
 export const addToCart = async (req: Request, res: Response) => {
-//   console.log(Product);
-
+  // res.send(Product)
   //* Requiere id del user y el id del producto (llegan desde query params)
-  //   const { idUser, idProduct , idCart} = req.query;
-  //   try {
-  //     // const user = await User.findByPk(idUser);
-  //     // const product = await Product.findByPk(idProduct);
-  //     // const cart = await Cart.findByPk(idCart)
-
-  //   } catch (error) {}
+  const { idUser, idProduct, idCart } = req.query;
+  try {
+    // const user = await User.findByPk(idUser);
+    const product = await Product.findByPk(idProduct);
+    const cart = await Cart.findByPk(idCart);
+    cart.addProducts(product);
+    // res.status(201).json(Cart.prototype);
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
 };

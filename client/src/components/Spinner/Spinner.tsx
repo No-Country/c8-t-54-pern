7fe015fa8f9faa824198d374667ce6c1c3310d82
adrianmcenturion <adrianmcenturion@gmail.com>
2022-11-28 16:02:14 -0300

    add spinner component and spinner to lazy loading

---
+++ client/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,16 @@
+type Props = {};
+const Spinner = (props: Props) => {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="pinwheel">
+        <div className="pinwheel__line"></div>
+        <div className="pinwheel__line"></div>
+        <div className="pinwheel__line"></div>
+        <div className="pinwheel__line"></div>
+        <div className="pinwheel__line"></div>
+        <div className="pinwheel__line"></div>  
+      </div>
+    </div>
+  );
+};
+export default Spinner;

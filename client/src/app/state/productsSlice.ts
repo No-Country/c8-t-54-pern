e678278673e37c93ae60df0b644e2d88769051d7
Dario Elguero <darioelguero@gmail.com>
2022-12-01 18:06:34 -0300

    unnecessary code removed

--- client/src/app/state/productsSlice.ts
+++ client/src/app/state/productsSlice.ts
@@ -1,17 +1,13 @@
-import { Dispatch } from "react";
 import { Product } from "./../../models/Products";
-import { AnyAction, createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { getRequest } from "../../services/httpRequest";
 
 export const initialState: Array<Product> = [];
 
 export const productSlice = createSlice({
   name: "products",
-  // initialState: {
-  //   list: [],
-  // },
   initialState: {
-    list: initialState
+    list: initialState,
   },
   reducers: {
     setProductsList: (state, action) => {
@@ -26,8 +22,7 @@ export default productSlice.reducer;
 
 export const getAllProducts = () => async (dispatch: any) => {
   try {
-    const {products} = await getRequest("/products")
-    console.log("PRODUCTS:", products);
+    const { products } = await getRequest("/products");
     dispatch(setProductsList(products));
   } catch (error) {
     console.log(error);

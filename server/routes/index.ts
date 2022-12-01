aab8bd3ad4dcd3374784badca1c540b81fc7f0aa
gaspar-costa-calvo <gaspar.c.calvo@gmail.com>
2022-12-01 12:12:34 -0300

    Category crud almost ready, delete not aviable yet

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express"
 import { Response, Request } from "express";
+import { categoryRouter } from "./categories.routes";
 import productRouter from "./products";
 import prodInCart from "./productsInCart";
 import userRouter from "./users.routes";
 const router = Router()
 
+router.use('/categories', categoryRouter)
 router.use("/users", userRouter);
 router.use('/products', productRouter);
 router.use('/cart', prodInCart)

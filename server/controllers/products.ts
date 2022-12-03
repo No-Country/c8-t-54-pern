95d129139809ae9016f7f497e5993e71febd6f40
Juan <juanm.fernandez@hotmail.com>
2022-12-03 01:05:14 -0300

    Fix asocc with change import to require

--- server/controllers/products.ts
+++ server/controllers/products.ts
@@ -1,7 +1,10 @@
-import { Product, ProductImgsAssoc } from "../models/Products";
+//import { Product, ProductImgsAssoc } from "../models/Products";
 import { Request, Response } from "express";
 import { getErrorMessage, reportError } from "../helpers/errorReport";
-import { Size } from "../models/Sizes";
+//import { Size } from "../models/Sizes";
+const { Product, ProductImgsAssoc } = require("../models/Products");
+const { User } = require("../models/Users");
+const { Size } = require("../models/Sizes");
 
 
 // List of every product in db
@@ -52,25 +55,14 @@ export const saveProduct = async (req: Request, res: Response) => {
                 as: "ProductImgs",
             }]
         })
+
         console.log("Product methods: ",Object.keys(Product.prototype))
-        console.log("Size methods: ",Object.keys(Size.prototype))
-        const np = await Product.findOne({
-            where: { id: newProduct.dataValues.id }
-        });
+
         const size = await Size.findOne({
             where: { id: sizeId }
         });
-        console.log("Product np: ",np)
-        console.log("Size size: ",size)
-        await np?.dataValues.addSize(size)
-        //     const product = await Product.findByPk(idProduct);
-        // const cart = await Cart.findByPk(idCart);
-        // if (product.quantityInStock > 0) {
-        // const productAdd = cart.addProducts(product);
-        // cart.increment({ totalPrice: product.price });
-        // res.status(200).json(productAdd);
-        // } 
-        //np?.setSizes()
+        newProduct.addSize(size)
+
         res.status(201).json({ message: "New Product created", newProduct })
     } catch (error) {
         res.status(400).json(reportError({ message: getErrorMessage(error) }))
